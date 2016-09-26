913248613d019ab21e7dfe32b0e1c827e440908a
Tom <279313838@qq.com>
2016-09-26 15:48:52 +0800

    frist dome

---
+++ app/src/main/java/zhuoxin/andriody/com/gitdroid/MyApplication.java
@@ -0,0 +1,24 @@
+package zhuoxin.andriody.com.gitdroid;
+
+import android.app.Application;
+
+import com.zhy.http.okhttp.OkHttpUtils;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+
+/**
+ * Created by Administrator on 9/26 0026.
+ */
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        OkHttpClient okHttpClient=new OkHttpClient.Builder()
+                .connectTimeout(10000L, TimeUnit.MILLISECONDS)
+                .readTimeout(10000L, TimeUnit.MILLISECONDS)
+                .build();
+        OkHttpUtils.initClient(okHttpClient);
+    }
+}
